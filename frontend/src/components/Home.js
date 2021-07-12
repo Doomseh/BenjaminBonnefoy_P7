b5bd4ec445ca9b02a6e69abb506931c7ac3978ef
Benjamin Bonnefoy <Benjamin.bonnefoy@hotmail.com>
2021-07-12 14:42:01 +0200

    début mise en place du home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,14 +1,26 @@
 import Post from "./Post";
+import useSWR from "swr";
 const token = localStorage.getItem("token");
 
 function Home() {
+    const { data, error } = useSWR("http://localhost:3000/api/posts/");
+    console.log(data)
+    
+    const renderPosts = (posts) => {
+        return posts.map(({ id, title, message, userId, createdAt }) => <Post key={id} id={id} title={title} message={message} userId={userId} createdAt={createdAt}/>
+    )}
+        
+        
+
+    if (error) return <div className="component title">failed to load</div>
+    if (!data) return <div className="component title">Aucune publication trouvée...</div>
     if (!token) return <div className="error">Vous n'êtes pas connecté !</div>
-    return (
-        <div className="component">
-            <h1 className="title">Publications</h1>
-            <Post id="234" userId="Martin" title="Je suis un titre" message="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum" createdAt="22/12/2321" />
-        </div>
-    )
+        return (
+            <div className="component">
+                <h1 className="title">Publications</h1>      
+                {renderPosts(data.data)}
+            </div>
+        )
 }
 
 export default Home;
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,22 +1,29 @@
 import test from "../assets/test.png";
 import {Link} from 'react-router-dom';
+//import useSWR from "swr";
 
-function Post({ id, userId, title, message, createdAt }) {
-    let linkId = "post?id=" + id
 
+function Post({id, title}) {
+    //const { data, error } = useSWR("http://localhost:3000/api/posts/");
+console.log(title)
+    //if (error) return <div className="component title">failed to load</div>
+    //if (!data) return <div className="component title">Aucune publication trouvée...</div>
     return (
-        <Link to={linkId} className="post-link">
+
+        <Link to="" className="post-link" key={id}>
             <div className="post">
                 <h2 className="post-title">{title}</h2>
                 <img src={test} alt="" className="post-img"></img>
-                <p className="post-message">{message}</p>
+                <p className="post-message">t</p>
                 <div className="post-user">
-                    <p className="post-time">Publié le : {createdAt}</p>
-                    <p className="post-name">Par : {userId}</p>
+                    <p className="post-time">Publié le : </p>
+                    <p className="post-name">Par : </p>
                 </div>
             </div>
         </Link>
-    )
+
+        )
+    
 }
 
 export default Post;
